a8ea65085cb4a32ab8178a724ae4c6eef8e8873f
LunaPresent <luna.present@outlook.com>
2025-01-19 23:34:54 +0100

    * cleaned up options.lua, added windows specific options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,26 +1,32 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
+-- visual options
 vim.opt.cursorline = true
-vim.opt.jumpoptions = "view"
 vim.opt.list = true
-vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.undofile = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
 
+-- control options
+vim.opt.jumpoptions = "view"
+vim.opt.mouse = "a"
+vim.opt.undofile = true
+vim.opt.foldmethod = "marker"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+-- file content options
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 0
 vim.opt.shiftwidth = 0
 vim.opt.expandtab = false
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
-vim.opt.scrolloff = 8
-
-vim.opt.foldmethod = "marker"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+-- windows options for neovide
+if vim.g.neovide then
+	vim.api.nvim_exec('language en_US', true)
+	vim.o.guifont = "NotoSansM Nerd Font:h11:#h-none"
+	vim.g.neovide_position_animation_length = 0
+	vim.g.neovide_scroll_animation_length = 0
+	vim.g.neovide_cursor_animation_length = 0
+end
