10d961016396c17d889fdd8bb33274ac94a89e20
LunaPresent <luna.present@outlook.com>
2025-02-13 18:34:49 +0100

    + nvim smartcolumn

---
+++ .config/nvim/lua/plugins/smartcolumn.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"m4xshen/smartcolumn.nvim",
+		opts = {
+			colorcolumn = "100",
+			disabled_filetypes = {
+				"dashboard",
+				"lazy",
+				"mason",
+				"help",
+				"checkhealth",
+				"lspinfo",
+				"noice",
+				"lazygit"
+			}
+		},
+	},
+}
