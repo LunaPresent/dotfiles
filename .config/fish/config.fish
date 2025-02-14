df85fe93267143227ea90dcec0d7ac13ac55130d
LunaPresent <luna.present@outlook.com>
2025-02-15 00:05:12 +0100

    feat(fzf): don't follow symlinks by default

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
 	set -gx EDITOR nvim
 	set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+	set -gx FZF_DEFAULT_COMMAND "fd -H ."
 end
