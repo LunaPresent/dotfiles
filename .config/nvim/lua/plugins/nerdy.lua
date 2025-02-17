db989bef2b46e25a18c9854732dd6957762f62cf
LunaPresent <luna.present@outlook.com>
2025-02-18 00:29:56 +0100

    feat(nvim): add nerdy

---
+++ .config/nvim/lua/plugins/nerdy.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"2kabhishek/nerdy.nvim",
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		cmd = "Nerdy",
+		keys = {
+			{
+				"<leader>fn",
+				"<cmd>Nerdy<cr>",
+				desc = "Find nerd icons",
+			},
+		},
+	},
+}
