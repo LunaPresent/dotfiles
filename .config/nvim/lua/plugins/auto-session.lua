b29d575b29371e3f2193908e3bf2235a8008e634
LunaPresent <luna.present@outlook.com>
2025-03-04 10:06:17 +0100

    feat(nvim): add keys for auto-session's SessionSearch

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -2,13 +2,15 @@ return {
 	{
 		"rmagatti/auto-session",
 		lazy = false,
-
 		opts = {
 			root_dir = vim.fn.stdpath("state") .. "/sessions/",
 			auto_restore = false,
 			suppressed_dirs = { "~/", "~/code", "~/Downloads", "/" },
 			bypass_save_filetypes = { "dashboard", "dbui", "dbout" },
 			close_unsupported_windows = true,
+		},
+		keys = {
+			{ "<leader>fs", "<cmd>SessionSearch<cr>", desc = "Pick session to load" }
 		},
 	},
 }
