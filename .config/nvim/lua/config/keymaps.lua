bcfb5b455532cff0b771be26e0a7a5cf001b30f9
LunaPresent <luna.present@outlook.com>
2025-03-22 11:39:02 +0100

    feat(nvim): add keybind for escaping visual mode with return

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -13,6 +13,7 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move cursor up" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("v", "<CR>", "<Esc>", { desc = "Escape visual mode" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up" })
