4dcd3a4d891a4d818d5037c01d5e982fcbc42569
LunaPresent <luna.present@outlook.com>
2025-03-22 11:37:58 +0100

    feat(nvim): add version wildcard to rainbow-delimiters

--- .config/nvim/lua/plugins/rainbow-delimiters.lua
+++ .config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"HiPhish/rainbow-delimiters.nvim",
+		version = "*",
 		config = function()
 			local rainbow_delimiters = require("rainbow-delimiters")
 			require("rainbow-delimiters.setup").setup({
