84e6b86bc7c90ddfe24df0adb4c2bab988f400ee
LunaPresent <github@lunapresent.net>
2025-05-09 10:26:56 +0200

    feat(fish): delete jj esc keymap

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,4 @@
 function fish_user_key_bindings
     if test "$__fish_active_key_bindings" = fish_vi_key_bindings
-        bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
     end
 end
