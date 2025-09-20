714063983b985139a63b74858e079dc1532c990c
Luna Present <code@lunapresent.net>
2025-09-20 13:37:20 +0200

    feat(nvim): rework blink.cmp config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -7,15 +7,19 @@ return {
 		},
 		event = { "InsertEnter", "CmdlineEnter" },
 		opts = {
-			appearance = {
-				nerd_font_variant = "mono",
-			},
 			completion = {
 				list = {
-					selection = { preselect = false, auto_insert = true },
+					selection = { preselect = true, auto_insert = false },
 					cycle = {
 						from_top = false,
+						from_bottom = false,
 					},
+				},
+				menu = {
+					auto_show = false,
+				},
+				ghost_text = {
+					enabled = true,
 				},
 			},
 			sources = {
@@ -35,20 +39,10 @@ return {
 			},
 			keymap = {
 				preset = "enter",
-				["<Tab>"] = {
-					function(cmp)
-						if cmp.snippet_active() then
-							return cmp.accept()
-						else
-							return cmp.select_and_accept()
-						end
-					end,
-					"snippet_forward",
-					"fallback"
-				},
 				["<C-y>"] = { "select_and_accept" },
+				["<C-CR>"] = { "select_and_accept" },
 				["<C-k>"] = { "select_prev", "fallback" },
-				["<C-j>"] = { "select_next", "fallback" },
+				["<C-j>"] = { "select_next", "show" },
 			},
 			cmdline = {
 				completion = {
@@ -61,7 +55,6 @@ return {
 				},
 				keymap = {
 					preset = "enter",
-					["<Tab>"] = { "select_and_accept" },
 					["<C-y>"] = { "select_and_accept" },
 					["<C-k>"] = { "select_prev", "fallback" },
 					["<C-j>"] = { "select_next", "fallback" },
