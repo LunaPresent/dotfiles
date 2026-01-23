b1f882aeda19bc335a1efbc324dc47b561f21d52
Luna Present <code@lunapresent.net>
2026-01-23 15:23:42 +0100

    feat(nvim): update leap config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,34 +1,26 @@
 -- visual options
-vim.opt.cursorline = true
-vim.opt.list = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.wrap = false
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.o.cursorline = true
+vim.o.list = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.scrolloff = 8
+vim.o.signcolumn = "yes"
 
 -- control options
-vim.opt.confirm = true
-vim.opt.jumpoptions = "view"
-vim.opt.mouse = "a"
-vim.opt.undofile = true
-vim.opt.foldmethod = "marker"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.o.confirm = true
+vim.o.jumpoptions = "view"
+vim.o.mouse = "a"
+vim.o.undofile = true
+vim.o.foldmethod = "marker"
+vim.o.ignorecase = true
+vim.o.smartcase = true
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions,globals"
+vim.o.swapfile = false
 
 -- file content options
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 0
-vim.opt.shiftwidth = 0
-vim.opt.expandtab = false
-vim.opt.smartindent = true
-
--- windows options for neovide
-if vim.g.neovide then
-	vim.api.nvim_exec('language en_US', true)
-	vim.o.guifont = "NotoSansM Nerd Font:h11:#h-none"
-	vim.g.neovide_position_animation_length = 0
-	vim.g.neovide_scroll_animation_length = 0
-	vim.g.neovide_cursor_animation_length = 0
-end
+vim.o.tabstop = 4
+vim.o.softtabstop = 0
+vim.o.shiftwidth = 0
+vim.o.expandtab = false
+vim.o.smartindent = true
--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -10,11 +10,12 @@ return {
 		keys = {
 			{ "s",  mode = { "n", "x", "o" }, desc = "Leap Forward to" },
 			{ "S",  mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-			{ "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+			{ "gs", mode = "n",               desc = "Leap from Windows" },
 		},
 		config = function()
-			local leap = require("leap")
-			leap.add_default_mappings(true)
+			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+			vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+			vim.keymap.set("n", "gs", "<Plug>(leap-from-window)")
 		end,
 	}
 }
