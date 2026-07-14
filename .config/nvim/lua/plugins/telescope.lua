9f2cfb2677bcf4813c7d27881b8366a865321910
Luna Present <code@lunapresent.net>
2026-07-14 09:13:18 +0200

    feat(nvim): add opencode plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		lazy = false,
-		branch = "0.1.x",
+		version = "*",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-ui-select.nvim",
