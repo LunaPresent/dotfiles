48477967e3fdab81351b614fdc90ee5025f7263e
Luna Present <code@lunapresent.net>
2026-07-14 10:06:41 +0200

    feat(nvim): make treesitter start command plugin agnostic

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -3,3 +3,16 @@ vim.filetype.add({
 		gotmpl = "gotmpl",
 	},
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(args)
+		local ft = args.match
+		local lang = vim.treesitter.language.get_lang(ft)
+		if not lang then return end
+
+		local ok = select(1, vim.treesitter.language.add(lang))
+		if not ok then return end
+
+		vim.treesitter.start(0, lang)
+	end,
+})
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,14 +16,6 @@ return {
 			}
 
 			ts.install(langs)
-
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = langs,
-				callback = function()
-					vim.treesitter.start()
-					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-				end,
-			})
 		end,
 	},
 	{
